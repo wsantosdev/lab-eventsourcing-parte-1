e84a0541f5466f872ecab050cfca1bb0b0a944ae
Vinicius Mamoré <vinicius.mamore@gmail.com>
2020-08-24 20:11:50 -0400

    creating extention to event emitted

---
+++ tests/Lab.EventSourcing.Tests/Extensions/AssertExtensions.cs
@@ -0,0 +1,17 @@
+﻿using Lab.EventSourcing.Core;
+using System;
+using System.Collections.Generic;
+
+namespace Lab.EventSourcing.Tests.Extensions
+{
+    public partial class Assert
+    {
+        public static void EventEmitted<T>(IEnumerable<IEvent> events) where T : IEvent
+        {
+            foreach (var @event in events)
+                if (@event.GetType() == typeof(T)) return;
+
+            throw new Exception("Event not emitted.");
+        }
+    }
+}
