f278f8ddc2abb8802b52c96473c4766777c00429
Vinicius Mamoré <vinicius.mamore@gmail.com>
2020-08-24 20:11:27 -0400

    add class assembly info to prevent RuntimeBinderException

---
+++ Lab.EventSourcing.PriceAlert/Properties/AssembyInfo.cs
@@ -0,0 +1,9 @@
+﻿using System.Runtime.CompilerServices;
+
+[assembly: InternalsVisibleTo("Lab.EventSourcing.Core")]
+namespace Lab.EventSourcing.PriceAlert.Properties
+{
+    public class AssembyInfo
+    {
+    }
+}
